55d0bb7783d1508c69e36c5e233090344ab811fd
alexi <alexi@ALEXIA>
2026-01-28 13:30:57 +0200

    Add project files.

---
+++ E-learning platform/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using E_learning_platform.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_learning_platform.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ E-learning platform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_learning_platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
